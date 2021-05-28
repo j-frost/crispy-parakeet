852ba9ca14beedc08ccfd665cdfe77f4b4608d0c
Julian Felix Rost <j-frost@a3re.net>
2021-05-28 22:25:54 +0200

    Try flask to open 8080

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import discord
+from flask import Flask
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -12,5 +13,14 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return
+
+
 if __name__ == "__main__":
     client.run(DISCORD_TOKEN)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
