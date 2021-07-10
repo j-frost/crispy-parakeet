e40a665b42eae8b9328bac91bb9f36c07b109baa
Julian Felix Rost <j-frost+github@a3re.net>
2021-07-10 23:29:14 +0000

    Remove old token

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from crispy_parakeet import CrispyParakeet
 from flask import Flask
 import threading
 
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') or "ODQ3NjAyNzk2NzMxNTY0MDcz.YLAdng.C2l8m2p7uFtr5JVA8t9LKiiXnNI"
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 
 app = Flask(__name__)
