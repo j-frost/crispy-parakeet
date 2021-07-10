6aa663610b4a73e02d77360375da102bdff47c1e
Julian Felix Rost <j-frost+github@a3re.net>
2021-07-10 23:39:29 +0000

    Use correct index for arguments

--- crispy_parakeet.py
+++ crispy_parakeet.py
@@ -9,14 +9,14 @@ class CrispyParakeet(discord.Client):
             return
 
         arguments = message.content.split(' ')
-        if len(arguments) < 4:
+        if len(arguments) < 5:
             await message.channel.send("I don't get it. There's not enough arguments here :?")
             return
 
-        command = arguments[0]
-        source_voice_channel = arguments[1]
-        target_1_voice_channel = arguments[2]
-        target_2_voice_channel = arguments[3]
+        command = arguments[1]
+        source_voice_channel = arguments[2]
+        target_1_voice_channel = arguments[3]
+        target_2_voice_channel = arguments[4]
 
         await message.channel.send(f'Alrighty ;) trying to {command} everyone from {source_voice_channel} randomly into {target_1_voice_channel} and {target_2_voice_channel}')
 
