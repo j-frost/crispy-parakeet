aaa16574a6916d80953adbfd22aa6a72b08c4228
Julian Felix Rost <j-frost+github@a3re.net>
2021-07-11 22:29:25 +0000

    Add getting channel objects

--- crispy_parakeet.py
+++ crispy_parakeet.py
@@ -6,7 +6,10 @@ from discord import VoiceChannel, Member
 
 class CrispyParakeet(discord.Client):
 
-    async def distribute(self, source: VoiceChannel, channel1: VoiceChannel, channel2: VoiceChannel):
+    async def distribute(self, src: str, chan1: str, chan2: str):
+        source = await self.fetch_channel(src)
+        channel1 = await self.fetch_channel(chan1)
+        channel2 = await self.fetch_channel(chan2)
         [team1, team2] = numpy.array_split(random.shuffle(source.members), 2)
         asyncio.run(move_all_to(team1, channel1))
         asyncio.run(move_all_to(team2, channel2))
